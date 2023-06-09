39f4222a45ee774a8da3d4c6fbd90871c07be6ca
Andrea Kreideweiss <andrea.kreideweiss@ost.ch>
2023-06-09 21:52:08 +0200

    Add todo views and addOrEdit functionality

--- Vorlage/controller/todos-controller.js
+++ Vorlage/controller/todos-controller.js
@@ -2,20 +2,23 @@ import { todoStore } from '../services/todo-store.js';
 
 export class TodoController {
     
-    createTodo = async (req, res) => {
-      await todoStore.add(req.body.title, req.body.importance, req.body.dueDate, req.body.description, req.body.completed);
-      res.redirect("/");
-    };
+  add = (req, res) => {
+    res.render("addOrEdit");
+};
 
-    showTodo = async (req, res) => {
-        let todo = await todoStore.get(req.params.id);
-        res.render("showTodo", { todo });
-    };
+showTodo = async (req, res) => {
+    let todo = await todoStore.get(req.params.id);
+    res.render("addOrEdit", { todo });
+};
 
-    updateTodo = async (req, res) => {
-        let updatedTodo = await todoStore.update(req.params.id, req.body.title, req.body.importance, req.body.dueDate, req.body.description, req.body.completed);
-        res.render("showTodo", { todo: updatedTodo });
-    };
+createOrUpdateTodo = async (req, res) => {
+    if (req.body.id) {
+        await todoStore.update(req.body.id, req.body.title, req.body.importance, req.body.dueDate, req.body.description, req.body.completed);
+    } else {
+        await todoStore.add(req.body.title, req.body.importance, req.body.dueDate, req.body.description, req.body.completed);
+    }
+    res.redirect("/");
+};
 
     deleteTodo = async (req, res) => {
         let deletedTodo = await todoStore.delete(req.params.id);
