4f025a0935c0a3cbc21e62ae1b1d44c718ac6266
Andrea Kreideweiss <andrea.kreideweiss@ost.ch>
2023-06-09 22:30:12 +0200

    Remove seed code

--- Vorlage/index.js
+++ Vorlage/index.js
@@ -7,12 +7,6 @@ import { todoStore } from './services/todo-store.js';
 
     // load app with current config
     const app = (await import('./app.js')).app;
-    async function createTodos() {
-        await todoStore.add("Pizza", 1, new Date(), "Order pizza", false);
-        await todoStore.add("Beer", 2, new Date(), "Order beer", false);
-        await todoStore.add("Finish assignment", 3, new Date(), "Finish web dev assignment", false);
-      }
-    createTodos();
     const hostname = '127.0.0.1';
     const port = 3001;
     app.listen(port, hostname, () => {
