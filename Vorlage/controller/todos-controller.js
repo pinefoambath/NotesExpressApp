b19b34564462f7d7aa65690e2a1b9fca9fc9a954
Andrea Kreideweiss <andrea.kreideweiss@ost.ch>
2023-07-09 12:53:58 +0200

    Add buttons and fix routing

--- Vorlage/controller/todos-controller.js
+++ Vorlage/controller/todos-controller.js
@@ -21,24 +21,27 @@ export class TodoController {
         res.render("addOrEdit", { todo });
     };
 
-showTodo = async (req, res) => {
-    let todo = await todoStore.get(req.params.id);
-    if (!todo) {
-      res.status(404).send('Todo not found 😜');
-    } else {
-        res.render("addOrEdit", { todo });
-    }
-  };
-
-createOrUpdateTodo = async (req, res) => {
-    if (req.body.id) {
-        await todoStore.update(req.body.id, req.body.title, req.body.importance, req.body.dueDate, req.body.description, req.body.completed);
-    } else {
-        await todoStore.add(req.body.title, req.body.importance, req.body.dueDate, req.body.description, req.body.completed);
-    }
-    res.redirect("/");
-};
-
+    showTodo = async (req, res) => {
+        let todo = await todoStore.get(req.params.id);
+        if (!todo) {
+        res.status(404).send('Todo not found 😜');
+        } else {
+            res.render("addOrEdit", { todo });
+        }
+    };
+ 
+    createOrUpdateTodo = async (req) => {
+        let todo;
+        let redirectPath;
+        if (req.body.id) {
+          todo = await todoStore.update(req.body.id, req.body.title, req.body.importance, req.body.dueDate, req.body.description, req.body.completed);
+          redirectPath = req.body['submit-type'] === 'Update' ? `/todos/${req.body.id}` : '/';
+        } else {
+          todo = await todoStore.add(req.body.title, req.body.importance, req.body.dueDate, req.body.description, req.body.completed);
+          redirectPath = req.body['submit-type'] === 'Create' ? `/todos/${todo._id}` : '/';
+        }
+        return { id: todo._id, redirect: redirectPath };
+    };
 }
 
 export const todoController = new TodoController();
