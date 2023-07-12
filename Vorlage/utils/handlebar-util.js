af1c07cfc1320a9d5353f121694146af59ce708f
Regina Traber <traber.regina@hispeed.ch>
2023-07-13 01:14:26 +0200

    adding filtering, sorting and change style functionality

--- Vorlage/utils/handlebar-util.js
+++ Vorlage/utils/handlebar-util.js
@@ -1,8 +1,23 @@
 export const helpers = {
-    'if_eq': function (a, b, opts) {
-        if (a === b)
-            return opts.fn(this);
-        else
-            return opts.inverse(this);
+  if_eq: function (a, b, opts) {
+    if (a === b) return opts.fn(this);
+    else return opts.inverse(this);
+  },
+  sortingDirectionIcon: function (context, sortBy, sortDirection) {
+    if (context === sortBy) {
+      if (sortDirection === -1) {
+        return "\u25B2";
+      } else {
+        return "\u25BC";
+      }
+    } else {
     }
-}
+  },
+  makeTextBoldIfSelected: function (text, filterOnCompleted) {
+    if (filterOnCompleted) {
+      return text + " TODO Style: text fett";
+    } else {
+      return text;
+    }
+  },
+};
