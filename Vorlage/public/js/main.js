5e29258d662a8c74a79fe23826527ce3ca9ad6b1
Andrea Kreideweiss <andrea.kreideweiss@ost.ch>
2023-07-14 19:21:32 +0200

    Refactor

--- Vorlage/public/js/main.js
+++ Vorlage/public/js/main.js
@@ -1,61 +1,67 @@
-document.querySelectorAll(".completed-checkbox").forEach((checkbox) => {
-  checkbox.addEventListener("change", function () {
-    const id = this.getAttribute("data-id");
-    const completed = this.checked;
-    fetch(`/todos/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ completed: completed }),
+function addCheckboxListeners() {
+  document.querySelectorAll(".completed-checkbox").forEach((checkbox) => {
+    checkbox.addEventListener("change", function () {
+      const id = this.getAttribute("data-id");
+      const completed = this.checked;
+      fetch(`/todos/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ completed: completed }),
+      });
     });
   });
-});
+}
 
-window.onload = function () {
-
+function setSessionStorageItems() {
   sessionStorage.setItem('sortBy', document.getElementById('sortBy').value);
   sessionStorage.setItem('sortDirection', document.getElementById('sortDirection').value);
+}
 
-  document.getElementById('sortByTitle').addEventListener('click', function () {
-      updateSort('title');
+function addSortEventListeners() {
+  const sortCriteria = ['title', 'dueDate', 'creationDate', 'importance'];
+
+  sortCriteria.forEach(criteria => {
+    document.getElementById(`sortBy${criteria.charAt(0).toUpperCase() + criteria.slice(1)}`).addEventListener('click', function () {
+      updateSort(criteria);
+    });
   });
+}
 
-  document.getElementById('sortByDueDate').addEventListener('click', function () {
-      updateSort('dueDate');
-  });
-
-  document.getElementById('sortByCreationDate').addEventListener('click', function () {
-      updateSort('creationDate');
-  });
-
-  document.getElementById('sortByImportance').addEventListener('click', function () {
-      updateSort('importance');
-  });
-
+function addToggleEventListeners() {
   document.getElementById('filterOnCompleted').addEventListener('click', function () {
-      window.location.href = '/toggle-completed-filter';
+    window.location.href = '/toggle-completed-filter';
   });
-  
+
   document.getElementById('changeStyle').addEventListener('click', function () {
-      window.location.href = '/toggle-dark-mode';
+    window.location.href = '/toggle-dark-mode';
   });
-};
+}
 
 function updateSort(sortBy) {
   const currentSortBy = sessionStorage.getItem('sortBy');
   const currentSortDirection = sessionStorage.getItem('sortDirection');
 
   if (sortBy === currentSortBy) {
-      const newSortDirection = currentSortDirection === 'asc' ? 'desc' : 'asc';
-      sessionStorage.setItem('sortDirection', newSortDirection);
+    const newSortDirection = currentSortDirection === 'asc' ? 'desc' : 'asc';
+    sessionStorage.setItem('sortDirection', newSortDirection);
   } else {
-      sessionStorage.setItem('sortBy', sortBy);
-      sessionStorage.setItem('sortDirection', 'asc');
+    sessionStorage.setItem('sortBy', sortBy);
+    sessionStorage.setItem('sortDirection', 'asc');
   }
 
   const newSortBy = sessionStorage.getItem('sortBy');
   const newSortDirection = sessionStorage.getItem('sortDirection');
   window.location.href = `/?sortBy=${newSortBy}&sortDirection=${newSortDirection}`;
 }
+
+// On page load, setze die Session Storage Items und füge die Event Listener hinzu
+window.onload = function () {
+  addCheckboxListeners();
+  setSessionStorageItems();
+  addSortEventListeners();
+  addToggleEventListeners();
+};
+
 
